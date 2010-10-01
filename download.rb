63718bcb4ba2181623f0012d159c6c2d4555abb8
Jon Fuller <fullerjc@gmail.com>
2010-10-01 09:52:54 -0400

    configurable download location

--- download.rb
+++ download.rb
@@ -42,8 +42,12 @@ def update_history(feed_history, last_timestamp, feed_url)
   new_history
 end
 
-def download_item(item, i)
-  logger.info "  downloading #{i+1}/#{to_download.size} (#{item.date})"
+def download_item(item, current, total, download_location, logger)
+  unless File.exists?(download_location)
+    Dir.mkdir(download_location)
+    logger.info "  Download location #{download_location} does not exist, creating..."
+  end
+  logger.info "  downloading #{current}/#{total} (#{item.date})"
 end
 
 logger = Logger.new('downloader.log', 'weekly')
@@ -56,6 +60,7 @@ from_history = hash_on('url', history['feeds'])
 
 feeds = from_config.each{|url, item| item.merge!(from_history[url] || {})}
 
+logger.info "download location: #{config['download_location']}"
 feeds.each do |feed_url, feed|
   logger.info "processing #{feed_url}"
 
@@ -65,7 +70,7 @@ feeds.each do |feed_url, feed|
   to_download = rss.items.select { |item| item.date > last_timestamp }
   logger.info "  #{to_download.size} items to download since #{last_timestamp}"
   to_download.reverse.each_with_index do |item, i|
-    download_items(item, i)
+    download_item(item, i+1, to_download.size, config['download_location'], logger)
   end
 
   if (to_download.any?)
