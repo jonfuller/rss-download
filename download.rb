32c2b8d6e687bbf165b22106de962d3255154464
Jon Fuller <fullerjc@gmail.com>
2010-10-01 08:07:18 -0400

    got some feed parsing working

--- download.rb
+++ download.rb
@@ -1,5 +1,7 @@
 require 'rubygems'
 require 'yaml'
+require 'rss'
+require 'open-uri'
 
 def load_yaml(filename)
   if File.exists? filename
@@ -20,7 +22,12 @@ from_config = hash_on('url', load_yaml('config.yml')['feeds'])
 from_history = hash_on('url', load_yaml('history.yml')['feeds'])
 
 feeds = from_config.each{|url, item| item.merge!(from_history[url] || {})}
+def read_feed(url)
+  content = open(url) { |s| s.read }
+  RSS::Parser.parse(content, false)
+end
 
-feeds.each do |show_url, show|
-  puts show_url, show.inspect
+feeds.each do |feed_url, feed|
+  rss = read_feed(feed_url)
+  puts feed_url, rss.items.first.date
 end
